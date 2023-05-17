32fcacfd47736c8ff0a9c7f98b556af804355071
Artem Farafonov <farafonov.artem@gene.com>
2023-05-17 20:19:23 +0300

    return headless mode

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 options = Options()
-#options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
